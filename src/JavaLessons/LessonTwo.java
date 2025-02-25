f7076bea80f876d619809f02d6deb09ea13b29e3
Code.in <amrdodani@gmail.com>
2025-02-25 09:53:29 -0600

    taking user inputs

--- src/JavaLessons/LessonTwo.java
+++ src/JavaLessons/LessonTwo.java
@@ -1,9 +1,20 @@
 package JavaLessons;
 
+import java.util.Scanner;	
+
 public class LessonTwo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.print("what is your name? :");
+			
+	String name = scanner.nextLine();		
+	
+	
+			
+	
 
 	}
 

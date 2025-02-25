98778fe93d3492425bca8cc1c4e744b634313271
Code.in <amrdodani@gmail.com>
2025-02-25 11:20:10 -0600

    completed uswer inputs

--- src/JavaLessons/LessonTwo.java
+++ src/JavaLessons/LessonTwo.java
@@ -16,18 +16,47 @@ public class LessonTwo {
 	
 	int age = scanner.nextInt();
 	
+	System.out.print(" what is you gender?: ");
+	
+	String gender = scanner.nextLine();
+	
+	System.out.print("Where are you from?: ");
+	
+	String from  = scanner.nextLine();
+	
+    System.out.print("Where do you live now?: ");
+	
+	String livenow  = scanner.nextLine();
+	
+	 System.out.print("Where do you work: ");
+		
+		String workplace  = scanner.nextLine();
+		
+       System.out.print("What is your job role?: ");
+		
+		String jobrole  = scanner.nextLine();
+		
+	
+       System.out.print("What is your salary?: ");
+		
+		int salary  = scanner.nextInt();
+		scanner.nextLine();
+		
+     System.out.print("How much raise are you getting next year?: ");
+		
+		int increase  = scanner.nextInt();
+		scanner.nextLine();
+		
+		int newsalary= salary+increase;
+		
+		System.out.println("My name is " + name+"." + "I am " + age +" years old, " + gender +" I work at " + workplace +
+				jobrole +"." + " I am currently live in " + livenow +"."+" i am orginaly from " + from +"."+"I make "+ salary +"." 
+				+" I will get " + increase +" as annual rasie so my total salary will would be"+ newsalary + "next year.");
+	
+	
 	scanner.nextLine();
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	scanner.close();
 
 	}
